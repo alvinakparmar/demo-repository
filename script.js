99ccd69346fa9033776d3b0830e249cf0d79ebf0
alvinakparmar <202304042.alvinakml@student.xavier.ac.in>
2026-01-16 12:33:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Simple JS to show a message when button is clicked
+document.getElementById("myButton").addEventListener("click", function() {
+    document.getElementById("message").textContent = "Hello! You clicked the button 🎉";
+});
